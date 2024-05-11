ba617d441a027779b7808fccbe8dd65690bc50b4
Shawon <shawonh986@gmail.com>
2024-05-11 14:52:03 +0800

    add decrement functionality also

--- Java Script/Counter App/counter.js
+++ Java Script/Counter App/counter.js
@@ -1,12 +1,20 @@
 let count = 0;
 
 const incrementButton = document.getElementById("increment");
+const decrementButton = document.getElementById("decrement");
 const displayElement = document.getElementById("counter");
 
 incrementButton.addEventListener("click",incrementCount);
 
 function incrementCount(){
     count += 1;
+    displayElement.innerText = count ;
+
+}
+
+decrementButton.addEventListener("click",decrementCount);
+function decrementCount(){
+    count -= 1;
     displayElement.innerText = count ;
 
 }
