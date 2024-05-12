8a3b1ec8377b0512ee20e6dafd05aadc952e8baf
Shawon <shawonh986@gmail.com>
2024-05-13 00:14:52 +0800

    add function

--- Java Script/Grade calculator/script.js
+++ Java Script/Grade calculator/script.js
@@ -2,25 +2,28 @@ const gradeValue = document.getElementById("firstValue");
 const buttons = document.getElementById("btns");
 const viewPlace = document.getElementById("result");
 
-function calculateGrade(){
-    const grade = gradeValue.value ;
-    if(grade>100 || grade<0){
-        viewPlace.innerText = "ভাল করে নাম্বার দেখে দে গরিব !!!!"
-    }else if(grade>=80){
-        viewPlace.innerText = "Your grade is A+"
-    }else if(grade>=70 && grade<80){
-        viewPlace.innerText = "Your grade is A"
-    }else if(grade>=60 && grade<70){
-        viewPlace.innerText = "Your grade is A-"
-    }else if(grade>=50 && grade<60){
-        viewPlace.innerText = "Your grade is B"
-    }else if(grade>=40 && grade<50){
-        viewPlace.innerText = "Your grade is C"
-    }else if(grade>=33 && grade<40){
-        viewPlace.innerText = "Your grade is D"
-    }else{
-        viewPlace.innerText = "তোমার জীবনটাই বেদনা, ফেইল???"
-    }
+function setDiaplayText(text){
+    viewPlace.innerText = text ; 
 }
 
-buttons.addEventListener('click',calculateGrade)
+
+buttons.addEventListener('click',function (){
+    const grade = gradeValue.value ;
+    if(grade>100 || grade<0){
+        setDiaplayText ("ভাল করে নাম্বার দেখে দে গরিব !!!!") 
+    }else if(grade>=80){
+        setDiaplayText ( "Your grade is A+")
+    }else if(grade>=70 && grade<80){
+        setDiaplayText("Your grade is A")
+    }else if(grade>=60 && grade<70){
+        setDiaplayText( "Your grade is A-")
+    }else if(grade>=50 && grade<60){
+        setDiaplayText("Your grade is B")
+    }else if(grade>=40 && grade<50){
+        setDiaplayText("Your grade is C")
+    }else if(grade>=33 && grade<40){
+        setDiaplayText( "Your grade is D")
+    }else{
+        setDiaplayText("তোমার জীবনটাই বেদনা, ফেইল???")
+    }
+})
