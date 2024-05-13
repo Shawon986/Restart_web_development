76f360f173692b197b5aff341b0ff43b32e7c4c5
Shawon <shawonh986@gmail.com>
2024-05-13 14:43:38 +0800

    design done

--- Java Script/Table tree app/script.js
+++ Java Script/Table tree app/script.js
@@ -2,9 +2,10 @@ const inputValue = document.getElementById("firstValue");
 const showButton = document.getElementById("btns");
 const resultPlace = document.getElementById("result");
 
-// resultPlace.innerText = '';
 
 function showTable(){
+    resultPlace.innerHTML = '';
+
     const input = parseInt(inputValue.value) ;
     for(let i=1; i<=10; i++){
         const tableRow = document.createElement("h4")
