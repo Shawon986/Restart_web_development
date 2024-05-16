d552c969cc2a65918ab5ae00d648fab9d7865e10
Shawon <shawonh986@gmail.com>
2024-05-16 10:59:15 +0800

    add increment part

---
+++ script1.js
@@ -0,0 +1,11 @@
+let count = 0;
+const counterDisplay = document.getElementById("counter");
+const plusButton = document.getElementById("increment");
+const minusButton = document.getElementById("decrement");
+
+function incrementCount(){
+    count +=1 ;
+    counterDisplay.innerText = count;
+}
+
+plusButton.addEventListener('click',incrementCount);
