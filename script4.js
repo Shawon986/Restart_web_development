c87c33e340c6130bc3998c2512c91bf55b822cba
Shawon <shawonh986@gmail.com>
2024-05-20 15:29:20 +0800

    add function

--- script4.js
+++ script4.js
@@ -2,29 +2,35 @@ const inputNumber = document.getElementById("input-number");
 const buttons = document.getElementById("generate");
 const displayArea = document.getElementById("table-container");
 
-function generateTable(){
-    const input = parseInt(inputNumber.value);
+function createTableFor(input){
     const table = document.createElement('table');
-    table.className = 'w-full border-collapse text-center'
-    displayArea.innerText = ''
+    table.className = 'w-full border border-collapse text-center';
+
     for(let i = 1;i<=10;i++){
         const tableRow = document.createElement('tr');
         tableRow.className = 'border-b'
 
         const leftCell = document.createElement('td');
-        leftCell.innerText = (`${input}*${i}`);
+        leftCell.style.border = '1px solid black'
+        leftCell.innerText = (`${input}*${i} =`);
 
         const rightCell = document.createElement('td');
+        rightCell.style.border = '1px solid black'
         rightCell.innerText = (`${input*i}`);
         
         tableRow.appendChild(leftCell);
         tableRow.appendChild(rightCell);
 
-        table.appendChild(tableRow)
-        
-        
+        table.appendChild(tableRow)  
 
     }
+    return table ;
+}
+
+function generateTable(){
+    const input = parseInt(inputNumber.value);
+    displayArea.innerText = ''
+    const table = createTableFor(input);
     displayArea.appendChild(table);
 }
 
