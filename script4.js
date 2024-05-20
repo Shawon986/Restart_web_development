fad97afc69600e73945b86a61cce89d4bd22ed0d
Shawon <shawonh986@gmail.com>
2024-05-20 14:59:26 +0800

    table making done

--- script4.js
+++ script4.js
@@ -4,14 +4,28 @@ const displayArea = document.getElementById("table-container");
 
 function generateTable(){
     const input = parseInt(inputNumber.value);
-    
+    const table = document.createElement('table');
+    table.className = 'w-full border-collapse text-center'
+    displayArea.innerText = ''
     for(let i = 1;i<=10;i++){
-        const tableRow = document.createElement('h4');
-        tableRow.innerText = (`${i}*${input}= ${i*input}`);
+        const tableRow = document.createElement('tr');
+        tableRow.className = 'border-b'
 
-        displayArea.appendChild(tableRow);
+        const leftCell = document.createElement('td');
+        leftCell.innerText = (`${input}*${i}`);
+
+        const rightCell = document.createElement('td');
+        rightCell.innerText = (`${input*i}`);
+        
+        tableRow.appendChild(leftCell);
+        tableRow.appendChild(rightCell);
+
+        table.appendChild(tableRow)
+        
+        
 
     }
+    displayArea.appendChild(table);
 }
 
 buttons.addEventListener('click',generateTable);
