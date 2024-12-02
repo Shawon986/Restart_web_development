562dd2f71faf615f8b7d9813ba67f75fa7e972b4
Shawon <shawonh986@gmail.com>
2024-12-02 22:13:38 +0800

    design done

--- Project_11/script.js
+++ Project_11/script.js
@@ -8,7 +8,7 @@ const resultDisplay = document.getElementById('result');
 function showResult(){
     const one = parseInt(firstInput.value);
     const two = parseInt(secondInput.value);
-
+ 
     if(one===two){
         resultDisplay.innerText = "2 numbers are same"
     }else if(one>two){
