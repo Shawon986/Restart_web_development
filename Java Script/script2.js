66b0be23f4911ef4121bb1b82dbb79a2e8d4eefb
Shawon <shawonh986@gmail.com>
2024-12-07 00:01:16 +0800

    for loop

--- Java Script/script2.js
+++ Java Script/script2.js
@@ -1,7 +1,5 @@
-let count = 0;
-while (count<=100){
-    if(count % 3===0){
-        console.log(count)
-    }
-    count = count + 1 ;
+for(let i =0; i <=10 ; i=i+2){
+    
+    console.log(i)
+    
 }
