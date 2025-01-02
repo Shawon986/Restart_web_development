181e534e887dbebc1b5e544197674422f3c7341c
Shawon <shawonh986@gmail.com>
2025-01-02 22:16:17 +0800

    add increament function

---
+++ javascript-practice/counter.js
@@ -0,0 +1,17 @@
+const displayArea = document.getElementById('display');
+
+const btn01 = document.getElementById('first');
+
+const btn02 = document.getElementById('second');
+
+let count = 0;
+
+function increase(){
+    count = count+ 1;
+    displayArea.innerText = count;
+    
+}
+
+btn01.addEventListener('click', increase);
+
+
