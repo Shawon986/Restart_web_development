e4d201ca7e59567e8392c38a04ae1aad7a93d073
Shawon <shawonh986@gmail.com>
2025-01-07 20:28:57 +0800

    grade function done

---
+++ Javascript practice 03/script.js
@@ -0,0 +1,25 @@
+const inputValue = document.getElementById('valueOne');
+const calculateButton = document.getElementById('first');
+const resetButton = document.getElementById('second');
+const displayResult = document.getElementById('showResult');
+
+
+calculateButton.addEventListener('click', calculateResult);
+
+function calculateResult(){
+    const mark = parseFloat(inputValue.value);
+    if(mark>=80 && mark<=100){
+        displayResult.innerText = 'A+';
+    } else if(mark>=70 && mark<=79){
+        displayResult.innerText = 'A';
+    } else if(mark>=60 && mark<=69){
+        displayResult.innerText = 'A-';
+    } else if(mark>=50 && mark<=59){
+        displayResult.innerText = 'B';
+    } else if(mark>=40 && mark<=49){
+        displayResult.innerText = 'C';
+    } 
+    else{
+        displayResult.innerText = 'Fail';
+    }
+}
