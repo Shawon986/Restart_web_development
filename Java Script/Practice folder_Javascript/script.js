fa497ba758e664b3c845deb9e67841ab68e8d1da
Shawon <shawonh986@gmail.com>
2025-02-18 17:30:44 +0800

    add function for comparator

---
+++ Java Script/Practice folder_Javascript/script.js
@@ -0,0 +1,20 @@
+const firstInputField = document.getElementById('inputOne');
+const secondInputField = document.getElementById('inputTwo');
+const resultArea = document.getElementById('resultDisplay');
+const compareBtn = document.getElementById('compare');
+const resetBtn = document.getElementById('reset');
+
+
+
+compareBtn.addEventListener('click', function(){
+    const valueOne = parseFloat(firstInputField.value);
+    const valueTwo = parseFloat(secondInputField.value);
+
+    if(valueOne>valueTwo){
+        resultArea.innerText = 'First number is bigger than Second number'
+    } else if (valueOne<valueTwo){
+        resultArea.innerText = 'Second number is bigger than First number'
+    } else{
+        resultArea.innerText = 'Both numbers are equal'
+    }
+})
