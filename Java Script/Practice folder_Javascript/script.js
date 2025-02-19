d81dc2334d824f24f473c4573eec6383d78991fb
Shawon <shawonh986@gmail.com>
2025-02-19 15:14:46 +0800

    validation done

--- Java Script/Practice folder_Javascript/script.js
+++ Java Script/Practice folder_Javascript/script.js
@@ -3,26 +3,38 @@ const displayGrade = document.getElementById('resultDisplay');
 const calculateButton = document.getElementById('compare');
 const resetButton = document.getElementById("reset");
 
+function isValid(){
+    if(!firstInput.value){
+        alert("Mark cannot be empty")
+        return false;
+    }
+    return true;
+    
+}
 
 calculateButton.addEventListener('click', function(){
+
+    if(!isValid()){
+        return;
+    }
     const inputValue = parseFloat(firstInput.value);
 
     if(inputValue<=0 || inputValue>100){
         displayGrade.innerText = "Enter valid number"
     }else if(inputValue>=80 && inputValue<=100){
-        displayGrade.innerText = "A+"
+        displayGrade.innerText = "Your grade is A+"
     }else if(inputValue>=70 && inputValue<=80){
-        displayGrade.innerText = "A"
+        displayGrade.innerText = "Your grade is A"
     }else if(inputValue>=60 && inputValue<=70){
-        displayGrade.innerText = "A-"
+        displayGrade.innerText = "Your grade is A-"
     }else if(inputValue>=50 && inputValue<=60){
-        displayGrade.innerText = "B"
+        displayGrade.innerText = "Your grade is B"
     }else if(inputValue>=40 && inputValue<=50){
-        displayGrade.innerText = "C"
+        displayGrade.innerText = "Your grade is C"
     }else if(inputValue>=33 && inputValue<=40){
-        displayGrade.innerText = "D"
+        displayGrade.innerText = "Your grade is D"
     }else{
-        displayGrade.innerText = "Fail"
+        displayGrade.innerText = "You are Fail"
     }
 })
 
