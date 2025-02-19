28d0ae0ce15ca117a528b0523ed09c2e8c27ca71
Shawon <shawonh986@gmail.com>
2025-02-19 14:59:49 +0800

    done grade calculate function

---
+++ Java Script/Practice folder_Javascript/script.js
@@ -0,0 +1,27 @@
+const firstInput = document.getElementById('inputOne');
+const displayGrade = document.getElementById('resultDisplay');
+const calculateButton = document.getElementById('compare');
+const resetButton = document.getElementById("reset");
+
+
+calculateButton.addEventListener('click', function(){
+    const inputValue = parseFloat(firstInput.value);
+
+    if(inputValue<=0 || inputValue>=100){
+        displayGrade.innerText = "Enter valid number"
+    }else if(inputValue>=80 && inputValue<=100){
+        displayGrade.innerText = "A+"
+    }else if(inputValue>=70 && inputValue<=80){
+        displayGrade.innerText = "A"
+    }else if(inputValue>=60 && inputValue<=70){
+        displayGrade.innerText = "A-"
+    }else if(inputValue>=50 && inputValue<=60){
+        displayGrade.innerText = "B"
+    }else if(inputValue>=40 && inputValue<=50){
+        displayGrade.innerText = "C"
+    }else if(inputValue>=33 && inputValue<=40){
+        displayGrade.innerText = "D"
+    }else{
+        displayGrade.innerText = "Fail"
+    }
+})
