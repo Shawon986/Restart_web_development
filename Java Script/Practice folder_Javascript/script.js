923ab06b826ff31eba999e554084bf38ce026206
Shawon <shawonh986@gmail.com>
2025-02-23 01:09:08 +0800

    loop implementation

--- Java Script/Practice folder_Javascript/script.js
+++ Java Script/Practice folder_Javascript/script.js
@@ -21,25 +21,24 @@ function generateTable(number){
 
 function generateRow(number,rowNo){
 
-    const cell1 = document.createElement('td')
-    const cell2 = document.createElement('td')
-    const cell3 = document.createElement('td')
-    const cell4 = document.createElement('td')
-    const cell5 = document.createElement('td')
+    const cells = [];
+    for(let i = 1; i<=5; i++){
+        const cell = document.createElement('td');
+        cells.push(cell);
+    }
 
     const tableRow = document.createElement('tr');
 
-    cell1.innerText = number;
-    cell2.innerText = "x";
-    cell3.innerText = rowNo;
-    cell4.innerText = "=";
-    cell5.innerText = number*rowNo;
+    cells[0].innerText = number;
+    cells[1].innerText = "x";
+    cells[2].innerText = rowNo;
+    cells[3].innerText = "=";
+    cells[4].innerText = number*rowNo;
 
-    tableRow.appendChild(cell1);
-    tableRow.appendChild(cell2);
-    tableRow.appendChild(cell3);
-    tableRow.appendChild(cell4);
-    tableRow.appendChild(cell5);
+    for(let i = 0; i<cells.length;i++){
+        tableRow.appendChild(cells[i])
+    }
+   
 
     return tableRow;
 
