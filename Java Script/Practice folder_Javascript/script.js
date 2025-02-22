cafc9072d30bca9ede8690310efbc030e8a67b39
Shawon <shawonh986@gmail.com>
2025-02-22 22:46:31 +0800

    add some loops for reducing repeated code

--- Java Script/Practice folder_Javascript/script.js
+++ Java Script/Practice folder_Javascript/script.js
@@ -25,24 +25,26 @@ function generateTable(num){
 }
 
 function generateRow(num,i){
-    const cell1 = document.createElement('td');
-    const cell2 = document.createElement('td');
-    const cell3 = document.createElement('td');
-    const cell4 = document.createElement('td');
-    const cell5 = document.createElement('td');
+    
+    const cells = [];
+    for(let i=1; i<=5; i++){
+        const cell = document.createElement('td');
+        cells.push(cell);
+    }
 
-    cell1.innerText = num;
-    cell2.innerText = "x";
-    cell3.innerText = i;
-    cell4.innerText = "=";
-    cell5.innerText = num*i;
+
+    cells[0].innerText = num;
+    cells[1].innerText = "x";
+    cells[2].innerText = i;
+    cells[3].innerText = "=";
+    cells[4].innerText = num*i;
 
     const tableRow = document.createElement('tr');
-    tableRow.appendChild(cell1);
-    tableRow.appendChild(cell2);
-    tableRow.appendChild(cell3);
-    tableRow.appendChild(cell4);
-    tableRow.appendChild(cell5);
+
+    for(let i =0; i<cells.length; i++){
+        tableRow.appendChild(cells[i])
+    }
+    
 
     return tableRow;
 }
