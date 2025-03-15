b7b59dd7cc096f4a7d879bbdcec603afa51db5ab
Shawon <shawonh986@gmail.com>
2025-03-16 00:00:36 +0800

    number comparator done

--- Java Script/Practice folder_Javascript/scratch.js
+++ Java Script/Practice folder_Javascript/scratch.js
@@ -1,3 +1,37 @@
+const inputOne = document.getElementById('num1');
+const inputTwo = document.getElementById('num2');
+const bnuttonOne = document.getElementById('reset');
+const buttonTwo = document.getElementById('compare');
+const display = document.getElementById('display');
+
+
+buttonTwo.addEventListener('click',compare);
+
+function compare(){
+    const num1 = parseInt(inputOne.value);
+    const num2 = parseInt(inputTwo.value);
+
+    if(num1 > num2){
+        display.innerText = `${num1} is greater than ${num2}`;
+    }if(num1 < num2){
+        display.innerText = `${num1} is less than ${num2}`;
+    }if(num1 === num2){
+        display.innerText = `${num1} is equal to ${num2}`;
+    }
+    
+
+};
+
+bnuttonOne.addEventListener('click',reset);
+
+function reset(){
+    inputOne.value = '';
+    inputTwo.value = '';
+    display.innerText = '';
+};
+
+
+
 
 
 
