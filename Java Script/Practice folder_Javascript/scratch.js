874cddc8a3daac831627a08dd234ed197450c5c2
Shawon <shawonh986@gmail.com>
2025-03-16 01:12:14 +0800

    grade calculator done

--- Java Script/Practice folder_Javascript/scratch.js
+++ Java Script/Practice folder_Javascript/scratch.js
@@ -6,12 +6,41 @@ const display = document.getElementById('display');
 
 
 
-bnuttonOne.addEventListener('click',reset);
+buttonTwo.addEventListener('click', calculate);
 
-function reset(){
-    inputOne.value = '';
-    display.innerText = '';
+function calculate() {
+    const number = parseInt(inputOne.value);
+    if(!number){
+        alert('Please enter a valid number');
+        return;
+    }
+    if(number<0 || number>100) {
+        display.innerText = 'Invalid number';
+        
+    }else if(number>=80){
+        display.innerText = 'A+';
+    }else if(number>=70){
+        display.innerText = 'A';
+    }else if(number>=60){
+        display.innerText = 'A-';   
+    }else if(number>=50){
+        display.innerText = 'B';
+    }else if(number>=40){
+        display.innerText = 'C';
+    }else if(number>=33){
+        display.innerText = 'D';
+    }else{
+        display.innerText = 'F';
+    }
 };
+
+
+bnuttonOne.addEventListener('click', reset);
+
+function reset() {
+    display.innerText = '';
+    inputOne.value = '';
+}
 
 
 
